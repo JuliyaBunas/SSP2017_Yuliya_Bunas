12fb460e578c8724560f527cf46be9c24520b5ae
JuliyaBunas <32575735+JuliyaBunas@users.noreply.github.com>
2017-12-01 00:59:41 +0300

    Add files via upload

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -10,218 +10,71 @@
  ********************************************************************************************/
 
 
-/**
- * Returns an area of a rectangle given by width and heigth.
- *
- * @param {numder} width
- * @param {number} height
- * @return {number}
- *
- * @example:
- *   5, 10 => 50
- *   5, 5  => 25
- */
-function getRectangleArea(width, height) {
-    throw new Error('Not implemented');
+function getRectangleArea(a,b){
+    return a*b;
+}
+function getCicleCircumference(a){
+   var circumference = 2*Math.PI*a;
+   return circumference;
+}
+function getAverage(a,b){
+    var x = (a+b)/2;
+    if(x == Infinity){
+        return Number.MAX_VALUE-1;
+    }else return x;
 }
 
-
-/**
- * Returns a circumference of circle given by radius.
- *
- * @param {number} radius
- * @return {number}
- *
- * @example:
- *   5    => 31.41592653589793
- *   3.14 => 19.729201864543903
- *   0    => 0
- */
-function getCicleCircumference(radius) {
-    throw new Error('Not implemented');
+function getDistanceBetweenPoints(a,b,c,d){
+    return Math.sqrt(Math.pow((c-a),2)+Math.pow((d-b),2));
 }
 
-/**
- * Returns an average of two given numbers.
- *
- * @param {numder} value1
- * @param {number} value2
- * @return {number}
- *
- * @example:
- *   5, 5  => 5
- *  10, 0  => 5
- *  -3, 3  => 0
- */
-function getAverage(value1, value2) {
-    throw new Error('Not implemented');
+function getLinearEquationRoot(a,b){
+    return (b*(-1))/a;   
 }
 
-/**
- * Returns a distance beetween two points by cartesian coordinates.
- *
- * @param {number} x1
- * @param {number} y1
- * @param {number} x2
- * @param {number} y2
- *
- * @return {number}
- *
- * @example:
- *   (0,0) (0,1)    => 1
- *   (0,0) (1,0)    => 1
- *   (-5,0) (10,-10) => 18.027756377319946
- */
-function getDistanceBetweenPoints(x1, y1, x2, y2) {
-    throw new Error('Not implemented');
+function getAngleBetweenVectors(a,b,c,d){
+    var x = a*c + b*d;
+    var y = Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
+    var z = Math.sqrt(Math.pow(c,2) + Math.pow(d,2));
+    var cos = x/(y*z);
+    var angle =Math.acos(cos);
+
+    return angle;
 }
 
-/**
- * Returns a root of linear equation a*x + b = 0 given by coefficients a and b.
- *
- * @param {number} a
- * @param {number} b
- * @return {number}
- *
- * @example:
- *   5*x - 10 = 0    => 2
- *   x + 8 = 0       => -8
- *   5*x = 0         => 0
- */
-function getLinearEquationRoot(a, b) {
-    throw new Error('Not implemented');
+function getLastDigit(a){
+   return a%10; 
 }
 
-
-/**
- * Returns an angle (in radians) between two vectors given by xi and yi, coordinates in Cartesian plane
- * See details https://en.wikipedia.org/wiki/Euclidean_vector#Representations
- *
- * @param {number} x1
- * @param {number} y1
- * @param {number} x2
- * @param {number} y2
- * @return {number}
- *
- * @example:
- *   (1,0) (0,1)     => π/2
- *   (0,1) (0,-1)    => π
- *   (0,-1) (1,0)    => π/2
- *   (0,1) (0,1)     => 0
- *   (0,1) (1,2)     => 0
- */
-function getAngleBetweenVectors(x1, y1, x2, y2) {
-    throw new Error('Not implemented');
+function parseNumberFromString(str){
+    return parseFloat(str);
 }
 
-/**
- * Returns a last digit of a integer number.
- *
- * @param {number} value
- * @return {number}
- *
- * @example:
- *   100     => 0
- *    37     => 7
- *     5     => 5
- *     0     => 0
- */
-function getLastDigit(value) {
-    throw new Error('Not implemented');
+function getParallelipidedDiagonal(a,b,c){
+    return Math.sqrt(Math.pow(a,2) + Math.pow(b,2) + Math.pow(c,2));
 }
 
-
-/**
- * Returns a number by given string representation.
- *
- * @param {string} value
- * @return {number}
- *
- * @example:
- *    '100'     => 100
- *     '37'     => 37
- * '-525.5'     => -525.5
- */
-function parseNumberFromString(value) {
-    throw new Error('Not implemented');
+function roundToPowerOfTen(a, b) {
+    return Math.round(a/Math.pow(10, b)) * Math.pow(10, b);
 }
 
-/**
- * Returns a diagonal length of the rectangular parallelepiped given by its sides a,b,c.
- *
- * @param {number} a
- * @param {number} b
- * @param {number} c
- * @return {number}
- *
- * @example:
- *   1,1,1   => 1.7320508075688772
- *   3,3,3   => 5.196152422706632
- *   1,2,3   => 3.741657386773941
- */
-function getParallelipidedDiagonal(a,b,c) {
-    throw new Error('Not implemented');
+function isPrime(a){
+    var flag = false;
+    for(var i = 2; i <= Math.sqrt(a); i++) {
+        if(a%i == 0) {
+            flag = true;
+            break;
+        }
+    }
+    return !flag;
 }
 
-/**
- * Returns the number rounded to specified power of 10.
- *
- * @param {number} num
- * @param {number} pow
- * @return {number}
- *  
- * @example:
- *   1234, 0  => 1234
- *   1234, 1  => 1230
- *   1234, 2  => 1200
- *   1234, 3  => 1000
- *   1678, 0  => 1678
- *   1678, 1  => 1680
- *   1678, 2  => 1700
- *   1678, 3  => 2000
- */
-function roundToPowerOfTen(num, pow) {
-    throw new Error('Not implemented');
-}
-
-/**
- * Returns true is the number is prime; otherwise false.
- * See: https://en.wikipedia.org/wiki/Primality_test
- *
- * @param {number} n
- * @return {bool}
- * 
- * @example:
- *   4 => false
- *   5 => true
- *   6 => false
- *   7 => true
- *   11 => true
- *   12 => false
- *   16 => false
- *   17 => true
- */
-function isPrime(n) {
-    throw new Error('Not implemented');
-}
-
-/**
- * Tries to convert value to number and returns it if conversion was successfull;
- * otherwise returns default value passed as a second argument.
- *
- * @param {any} value
- * @param {any} def
- * @return {number}
- *
- * @example
- *   toNumber(null, 0) => 0
- *   toNumber('test', 0) => 0
- *   toNumber('1', 0) => 1
- *   toNumber(42, 0) => 42
- *   toNumber(new Number(42), 0) => 42
- */
-function toNumber(value, def) {
-    throw new Error('Not implemented');
+function toNumber(a,b){
+    if(Number(a)){
+        return a;
+    }else{
+        return b;
+    }
 }
 
 module.exports = {
