d34643794604dfd1f92a03e19693fde574cfbc9a
JuliyaBunas <32575735+JuliyaBunas@users.noreply.github.com>
2017-12-28 17:53:48 +0200

    Add files via upload

--- task/11-katas-2-tasks.js
+++ task/11-katas-2-tasks.js
@@ -34,7 +34,44 @@
  *
  */
 function parseBankAccount(bankAccount) {
-    throw new Error('Not implemented');
+    let arr = bankAccount.split('\n');
+    let accNum = 0;
+    let numOfDigits = 9;
+    for (let k = 0; k < numOfDigits; k++) {
+        let digit;
+        let i = k * 3;
+        if(arr[2][i + 2] === ' ') {
+            digit = 2;
+        } else if(arr[1][i + 2] === ' ') {
+            if (arr[2][i] === ' ') {
+                digit = 5;
+            } else {
+                digit = 6;
+            }
+        }  else if(arr[2][i] === '|') {
+            if (arr[1][i + 1] === ' ') {
+                digit = 0;
+            } else {
+                digit = 8;
+            }
+        } else if(arr[0][i + 1] === ' ') {
+            if (arr[1][i + 1] === ' ') {
+                digit = 1;
+            } else {
+                digit = 4;
+            }
+        } else if(arr[1][i + 1] === '_') {
+            if (arr[1][i] === '|') {
+                digit = 9;
+            } else {
+                digit = 3;
+            }
+        } else {
+            digit = 7;
+        }
+        accNum += digit * Math.pow(10, numOfDigits - k - 1);
+    }
+    return accNum;
 }
 
 
@@ -63,7 +100,17 @@ function parseBankAccount(bankAccount) {
  *                                                                                                'characters.'
  */
 function* wrapText(text, columns) {
-    throw new Error('Not implemented');
+    let words = text.split(' ');
+    let line = '';
+    while (words.length) {
+        if(line.length + words[0].length < columns) {
+            line += line.length ? ' ' + words.shift() : words.shift();
+        } else {
+            yield line;
+            line = '';
+        }
+    }
+    yield line;
 }
 
 
@@ -97,11 +144,113 @@ const PokerRank = {
     TwoPairs: 2,
     OnePair: 1,
     HighCard: 0
-}
+};
+
+let nums = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+let signes = ['♣','♦','♥','♠'];
 
 function getPokerHandRank(hand) {
-    throw new Error('Not implemented');
+    let cards = [];
+    for (let card of hand) {
+        cards.push(parseRank(card));
+    }
+    let signSame = isSignSame(cards);
+    let straight = isStraight(cards);
+    let freq = frequencyCounting(cards);
+
+    if (signSame && straight)
+        return PokerRank.StraightFlush;
+    if (freq.four === 1)
+        return PokerRank.FourOfKind;
+    if (freq.three === 1 && freq.pairs === 1)
+        return PokerRank.FullHouse;
+    if (signSame)
+        return PokerRank.Flush;
+    if (straight)
+        return PokerRank.Straight;
+    if (freq.three === 1)
+        return PokerRank.ThreeOfKind;
+    if(freq.pairs === 2)
+        return PokerRank.TwoPairs;
+    if(freq.pairs === 1)
+        return PokerRank.OnePair;
+    else
+        return PokerRank.HighCard;
 }
+
+/**
+ *
+ * @param {string} value
+ * @returns {[number,number]}
+ */
+function parseRank(value){
+
+    let num = value.slice(0,-1);
+    let sign = value.slice(-1);
+    let num_i = nums.indexOf(num);
+    let sign_i = signes.indexOf(sign);
+    return [num_i,sign_i];
+}
+
+/**
+ *
+ * @param {Array} cards
+ * @returns {boolean}
+ */
+function isSignSame(cards){
+    for(let i=0;i<cards.length-1;i++){
+        if(cards[i][1] !== cards[i+1][1])
+            return false;
+    }
+    return true;
+}
+
+/**
+ *
+ * @param {Array} cards
+ * @returns {boolean}
+ */
+function isStraight(cards){
+    let values = cards.map((x)=>x[0]).filter(x=>x !== 0); //cut out A because of double behaviors
+    values.sort((x,y)=>x > y);
+    for(let i=0;i<values.length-1;i++){
+        if(values[i+1] !== values[i]+1)
+            return false;
+    }
+    if(values.length === 4){  //if A was cut, we choose which rank is A, low or high
+        return values[0] === 1 || values[values.length - 1] === 12;
+    }
+    return true;
+}
+
+/**
+ *
+ * @param {Array} cards
+ * @returns {{pairs: number, three: number, four: number}}
+ */
+function frequencyCounting(cards){
+    let values = cards.map((x)=>x[0]);
+    values.sort();
+    let map = new Map();
+    for(let i=0;i<values.length;i++){
+        if(map.has(values[i]) === false)
+            map.set(values[i],1);
+        else
+            map.set(values[i],map.get(values[i])+1);
+    }
+
+    let res = new Array(5).fill(0);
+    for (let it of map) {
+        res[it[1]]++;
+    }
+
+    return {
+        pairs: res[2],
+        three: res[3],
+        four: res[4]
+    };
+}
+
 
 
 /**
@@ -135,8 +284,76 @@ function getPokerHandRank(hand) {
  *    '+-------------+\n'
  */
 function* getFigureRectangles(figure) {
-   throw new Error('Not implemented');
+    let arr = figure.split('\n');
+    for(let i=0;i<arr.length;i++){
+        for(let j=0;j<arr[i].length;j++){
+            if(arr[i][j] === '+'){
+                let rect = detectRectangle(arr, i, j);
+
+                if (rect !== null){
+                    yield drawRectangle(rect.weight, rect.height)
+                }
+            }
+        }
+    }
 }
+
+/**
+ *
+ * @param {Array} figure
+ * @param {number} fromRowIndex
+ * @param {number} fromColumnIndex
+ *
+ * @return  {x: number, y: number} basic parts
+ */
+function detectRectangle(figure, fromRowIndex, fromColumnIndex){
+
+    for(let i=fromRowIndex+1;i<figure.length;i++){
+
+        if(figure[i][fromColumnIndex] === '+'){
+            for(let j=fromColumnIndex+1;j<figure[fromRowIndex].length;j++){
+                if(figure[i][j] === "+"){
+                    if(figure[fromRowIndex][j] === "+"){
+
+                        let flag = true;
+
+                        for(let k=fromRowIndex+1;k<i;k++) {
+                            if(figure[k][j] !== '|') {
+                                flag = false;
+                                break;
+                            }
+                        }
+
+                        if(flag)
+                            return {height: (i-fromRowIndex + 1), weight: (j-fromColumnIndex + 1)};
+                    }
+                }
+                else if(figure[i][j] !== '-'){
+                    break;
+                }
+            }
+        }
+        else if(figure[i][fromColumnIndex] !== '|')
+            break;
+
+    }
+
+    return null;
+}
+
+/**
+ *
+ * @param {number} weight
+ * @param {number} height
+ *
+ * @return {string}
+ */
+function drawRectangle(weight, height){
+    return '+' + '-'.repeat(weight - 2) + '+\n' +
+        ('|' + ' '.repeat(weight - 2) + '|\n').repeat(height - 2) +
+        '+' + '-'.repeat(weight - 2) + '+\n'
+}
+
 
 
 module.exports = {
--- task/12-katas-3-tasks.js
+++ task/12-katas-3-tasks.js
@@ -28,7 +28,46 @@
  *   'NULL'      => false 
  */
 function findStringInSnakingPuzzle(puzzle, searchStr) {
-    throw new Error('Not implemented');
+
+    puzzle = puzzle.map(item => item.split(''));
+
+    function dfs(currCoordinates, step) {
+        let save = puzzle[currCoordinates.x][currCoordinates.y];
+        puzzle[currCoordinates.x][currCoordinates.y] = "";
+
+        if (step === (searchStr.length-1))
+            return true;
+
+        let result = false;
+        let steps = [[1, 0], [-1, 0], [0, -1], [0, 1]];
+
+        for (let i = 0; i < 4; i++) {
+            let newX = currCoordinates.x + steps[i][0],
+                newY = currCoordinates.y + steps[i][1];
+            try {
+                if (puzzle[newX][newY] === searchStr[step+1]) {
+                    result = result || dfs({x: newX, y: newY}, step + 1);
+                    if(result){
+                        break;
+                    }
+                }
+            }
+            catch (e){}
+        }
+
+        puzzle[currCoordinates.x][currCoordinates.y] = save;
+        return result;
+    }
+
+    for (let i = 0; i < puzzle.length; i++) {
+        for (let j = 0; j < puzzle[i].length; j++)
+            if (puzzle[i][j] === searchStr[0]) {
+                if (dfs({x: i, y: j}, 0))
+                    return true;
+            }
+    }
+
+    return false;
 }
 
 
@@ -45,7 +84,24 @@ function findStringInSnakingPuzzle(puzzle, searchStr) {
  *    'abc' => 'abc','acb','bac','bca','cab','cba'
  */
 function* getPermutations(chars) {
-    throw new Error('Not implemented');
+    /**
+     * Heap's algorithm implementation
+     * @param {Array} a
+     * @param {number} n
+     */
+    function *permute(a, n) {
+        if (n < 2)
+            yield a.join('');
+        else
+            for (let i = 0; i < n; i++) {
+                yield *permute(a, n - 1);
+                const toSwap = n % 2 ? 0 : i;
+                let temp = a[n - 1];
+                a[n - 1] = a[toSwap];
+                a[toSwap] = temp;
+            }
+    }
+    yield *permute(chars.split(''), chars.length);
 }
 
 
@@ -65,7 +121,12 @@ function* getPermutations(chars) {
  *    [ 1, 6, 5, 10, 8, 7 ] => 18  (buy at 1,6,5 and sell all at 10)
  */
 function getMostProfitFromStockQuotes(quotes) {
-    throw new Error('Not implemented');
+    let profitArray = quotes.map((currentValue, index) =>
+        (quotes.slice(index, quotes.length)
+            .sort((a, b) => b - a)[0] - currentValue)
+    );
+
+    return profitArray.reduce((accum, value) => {return accum + value});
 }
 
 
@@ -92,13 +153,37 @@ function UrlShortener() {
 UrlShortener.prototype = {
 
     encode: function(url) {
-        throw new Error('Not implemented');
+        let result = "";
+
+        for (let i = 0; i < url.length; i += 2) {
+            let a = url.charCodeAt(i);
+            let b = url.charCodeAt(i + 1);
+            let code = (a << 8) | b;
+
+            result += String.fromCharCode(code);
+        }
+
+        return result;
     },
     
     decode: function(code) {
-        throw new Error('Not implemented');
-    } 
-}
+        let result = "";
+
+        for (let i = 0; i < code.length; i++) {
+            let char = parseInt(code.charCodeAt(i), 10);
+            let b = char & 255;
+            let a = (char >> 8) & 255;
+
+            if (b === 0) {
+                result += String.fromCharCode(a)
+            } else {
+                result += String.fromCharCode(a) + String.fromCharCode(b);
+            }
+        }
+
+        return result;
+    }
+};
 
 
 module.exports = {
